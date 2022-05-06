b1be0b1083327b5e98be5b1e87764227b8c03924
Romāns Potašovs <justgook@gmail.com>
2022-05-06 11:44:50 +0300

    fish clean up

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -1,13 +1,20 @@
-require'nvim-treesitter.configs'.setup {
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+
+if not status_ok then
+  return
+end
+
+configs.setup {
   -- A list of parser names, or "all"
   ensure_installed = { "go", "javascript", "lua", "fish", "lua" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
   -- List of parsers to ignore installing (for "all")
-  -- ignore_install = { "javascript" },
-
+  ignore_install = { "" },
+  autopairs = {
+    enable = true,
+  },
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
@@ -23,5 +30,10 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+  },
+  indent = { enable = true, disable = { "yaml" } },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
   },
 }
