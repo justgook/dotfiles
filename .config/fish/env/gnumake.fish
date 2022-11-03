c8df6aa76b4e72824ea7e19ee03428945bc3c143
Romāns Potašovs <justgook@gmail.com>
2022-11-03 14:29:25 +0200

    ⚰️ Remove otudated configs

--- .config/fish/env/gnumake.fish
+++
@@ -1,2 +0,0 @@
-#GNU Make
-fish_add_path /usr/local/opt/make/libexec/gnubin -P
--- .config/fish/env/go.fish
+++
@@ -1,7 +0,0 @@
-# Go (if go exists)
-if [ -f /usr/local/go/bin/go ]
-    set -g fish_user_paths "/usr/local/go/bin" fish_user_paths
-    set -x  GOPATH   $(go env GOPATH)
-    set -x PATH $PATH $GOPATH/bin
-    set -x PATH $PATH (go env GOPATH)/bin
-end
