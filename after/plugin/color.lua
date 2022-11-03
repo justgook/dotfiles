65648efa619bb3d1c9058b28f4400943ea2296eb
Romāns Potašovs <justgook@gmail.com>
2022-11-03 18:18:41 +0200

    💄 cleanup some style

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,5 +1,13 @@
-vim.g.tokyonigth_transparent_sidebar = true
-vim.g.tokyonigth_transparent = true
 vim.opt.background = "dark"
-
+require("tokyonight").setup({
+    transparent = false,
+})
 vim.cmd("colorscheme tokyonight")
+
+
+-- vim.g.sonokai_style = 'andromeda'
+-- vim.g.sonokai_better_performance = 1
+-- vim.g.sonokai_transparent_background = 2
+-- vim.g.sonokai_enable_italic = 1
+-- vim.g.sonokai_diagnostic_line_highlight = 1
+-- vim.cmd("colorscheme sonokai")
--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -6,16 +6,31 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+    -- Theme / colors / icons
     use 'folke/tokyonight.nvim'
+    use 'nvim-tree/nvim-web-devicons'
+
     use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
-
     use {
         "windwp/nvim-autopairs",
         config = function() require("nvim-autopairs").setup {} end
     }
+
     use 'nvim-treesitter/nvim-treesitter'
     use 'nvim-treesitter/nvim-treesitter-context'
+
+   -- use {
+   --     "folke/twilight.nvim",
+   --     config = function()
+   --         require("twilight").sestup {
+   --             -- `Twilight` : toggle twilight
+   --             -- your configuration comes here
+   --             -- or leave it empty to use the default settings
+   --             -- refer to the configuration section below
+   --         }
+   --     end
+   -- }
 
     --Fuzzy Finding
     use 'nvim-lua/telescope.nvim'
