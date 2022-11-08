4a79c3bfd822b51752e72675250b8e76c8db5403
Romāns Potašovs <justgook@gmail.com>
2022-11-08 12:20:20 +0200

    ⚡️ Update go bin dir

--- env/go.fish
+++ env/go.fish
@@ -1,7 +1,5 @@
-# Go (if go exists)
-if [ -f /usr/local/go/bin/go ]
-    set -g fish_user_paths "/usr/local/go/bin" fish_user_paths
-    set -x  GOPATH   $(go env GOPATH)
-    set -x PATH $PATH $GOPATH/bin
-    set -x PATH $PATH (go env GOPATH)/bin
+if [ -x "$(command -v go)" ] 
+    set -x GOPATH $(go env GOPATH)
+    fish_add_path  $(go env GOPATH)/bin -P
 end
+
